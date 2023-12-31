60f48a31270694dae6d0d911a12728eeac4c253c
angga-22 <anggasp.unib@gmail.com>
2023-12-31 22:55:19 +0700

    initial init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active
+const nav = document.querySelector(".nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  nav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+// document.addEventListener('click', function (e) {
+//   if(!hamburger.contains(e.target) && !nav)
+// })
+
+console.log("KONTOL NIH");
